5def76221558e7ab77d1fcb1ebca5746ba9b6a8d
Jorgen Schumann <jorgen@hotmail.de>
2018-06-02 11:33:59 +0200

    add infrastructure projects, add testprojects

---
+++ Backend/InitialEnterprise.Domain.MainBoundedContext.Api.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace InitialEnterprise.Domain.MainBoundedContext.Api.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
