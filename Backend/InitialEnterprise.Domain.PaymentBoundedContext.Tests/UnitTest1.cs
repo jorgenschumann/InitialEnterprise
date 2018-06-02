b8e6be2968b1a08d49db0df85291069e32322f1f
Jorgen Schumann <jorgen@hotmail.de>
2018-06-02 11:00:35 +0200

    add testprojects

---
+++ Backend/InitialEnterprise.Domain.PaymentBoundedContext.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace InitialEnterprise.Domain.PaymentBoundedContext.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
