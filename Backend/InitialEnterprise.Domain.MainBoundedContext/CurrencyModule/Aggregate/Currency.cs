a8f31ce727b669c81d2e0debb4fb18f1cc886654
Jorgen Schumann <jorgen@hotmail.de>
2018-06-02 14:22:38 +0200

    add currency member

---
+++ Backend/InitialEnterprise.Domain.MainBoundedContext/CurrencyModule/Aggregate/Currency.cs
@@ -0,0 +1,14 @@
+﻿using InitialEnterprise.Domain.SharedKernel;
+
+namespace InitialEnterprise.Domain.MainBoundedContext.CurrencyModule.Aggregate
+{
+    public class Currency : BaseEntity
+    {
+        public string Name { get; private set; }
+
+        public string IsoCode { get; private set; }
+
+        public decimal Rate { get; private set; }
+      
+    }
+}
