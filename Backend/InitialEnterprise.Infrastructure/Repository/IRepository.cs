1eb76de1cf6d78ccd9fcdff78f2b256533cea0ab
Jorgen Schumann <jorgen@hotmail.de>
2018-06-03 12:37:26 +0200

    add repository interfaces

---
+++ Backend/InitialEnterprise.Infrastructure/Repository/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using InitialEnterprise.Domain.SharedKernel;
+
+namespace InitialEnterprise.Infrastructure.Repository
+{
+
+    public interface IRepository<TIdentity, TEntity> :
+       IReadOnlyRepository<TIdentity, TEntity>,
+       ISaveOnlyRepository<TEntity>,
+       IDeleteOnlyRepository<TIdentity, TEntity> where TEntity : IEntity
+    {
+    }
+
+    public interface IReadOnlyRepository<TIdentity, TEntity> : IDisposable where TEntity : IEntity
+    {
+        TEntity Read(TIdentity Id);
+        IQueryable<TEntity> List(TIdentity Id);
+    }
+
+    public interface ISaveOnlyRepository<TEntity> : IDisposable where TEntity : IEntity
+    {
+        void Save(TEntity entity);
+        void Save(IEnumerable<TEntity> entities);
+    }
+
+    public interface IDeleteOnlyRepository<TIdentity, TEntity> : IDisposable where TEntity : IEntity
+    {
+        TEntity Delete(TEntity entity);       
+    }  
+}
