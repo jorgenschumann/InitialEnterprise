177289aa13a20263c6fd0a3af081402a4df40f32
Jorgen Schumann <jorgen@hotmail.de>
2018-06-03 08:52:19 +0200

    add moq setup in currency service tests, move ddd stuff to infrastructure

--- Backend/InitialEnterprise.Domain.MainBoundedContext.Tests/CurrencyModule/CurrencyServiceTests.cs
+++ Backend/InitialEnterprise.Domain.MainBoundedContext.Tests/CurrencyModule/CurrencyServiceTests.cs
@@ -1,18 +1,24 @@
 ﻿using System.Linq;
+using InitialEnterprise.Domain.MainBoundedContext.CurrencyModule.Repository;
 using InitialEnterprise.Domain.MainBoundedContext.CurrencyModule.Services;
 using InitialEnterprise.Testfixture;
+using Moq;
 using Xunit;
 
 namespace InitialEnterprise.Domain.MainBoundedContext.Tests.CurrencyModule
 {
     public class CurrencyServiceTests
     {
+       
         [Fact]
         public void Should_Save_Created_Currency()
         {
             //Arr
-            var expected = TestdataFactory.Currencies().First();  
-            var currencyService = new CurrencyService();
+            var expected = TestdataFactory.Currencies().First();
+            var mockRepository = new Mock<ICurrencyRepository>();
+            mockRepository.Setup(x => x.Save(expected)).Returns(expected);
+            
+            var currencyService = new CurrencyService(mockRepository.Object);
 
             //Act      
             var actual = currencyService.Save(expected);
---
+++ Backend/InitialEnterprise.Infrastructure/CQRS/Command/ICommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InitialEnterprise.Domain.SharedKernel
+{
+    public interface ICommand: IValidatable
+    {
+    }
+}
