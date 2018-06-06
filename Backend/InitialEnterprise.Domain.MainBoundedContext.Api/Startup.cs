d3f758fb9f0a924af133bd3a57943cd4cad788d2
Jorgen Schumann <jorgen@hotmail.de>
2018-06-06 20:56:04 +0200

    add simple currency controller , basic startup

--- Backend/InitialEnterprise.Domain.MainBoundedContext.Api/Startup.cs
+++ Backend/InitialEnterprise.Domain.MainBoundedContext.Api/Startup.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace InitialEnterprise.Domain.MainBoundedContext.Api
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -25,10 +18,7 @@ namespace InitialEnterprise.Domain.MainBoundedContext.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
