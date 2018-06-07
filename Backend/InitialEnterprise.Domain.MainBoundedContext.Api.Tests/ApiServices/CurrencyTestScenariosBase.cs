26e4ba9df58278e4f4958379a2e044b021f630b0
Jorgen Schumann <jorgen@hotmail.de>
2018-06-07 07:49:48 +0200

    add currency post integration test, abstract intergration test fixture

---
+++ Backend/InitialEnterprise.Domain.MainBoundedContext.Api.Tests/ApiServices/CurrencyTestScenariosBase.cs
@@ -0,0 +1,20 @@
+﻿namespace InitialEnterprise.Domain.MainBoundedContext.Api.Tests
+{
+    public class CurrencyTestScenariosBase : TestScenariosBase
+    {
+        private const string ApiUrlBase = "api/currency";
+                        
+        public static class Get
+        {
+            public static string GetCurrency(int id)
+            {
+                return $"{ApiUrlBase}/{id}";
+            }
+        }
+
+        public static class Post
+        {
+            public static string Currency = $"{ApiUrlBase}/";
+        }
+    }
+}
---
+++ Backend/InitialEnterprise.Domain.MainBoundedContext.Api.Tests/ApiServices/TestScenariosBase.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+namespace InitialEnterprise.Domain.MainBoundedContext.Api.Tests
+{
+    public abstract class TestScenariosBase
+    {
+        public TestServer CreateServer(string directory)
+        {
+            var webHostBuilder = WebHost.CreateDefaultBuilder();
+            {
+                webHostBuilder.UseContentRoot(Directory.GetCurrentDirectory() + $"\\{directory}");
+                webHostBuilder.UseStartup<Startup>();
+            }
+            return new TestServer(webHostBuilder);
+        }
+    }
+}
