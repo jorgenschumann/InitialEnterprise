07c084a3b95ffdeb642a5fab137e34a5edc47711
Jorgen Schumann <jorgen@hotmail.de>
2018-06-21 21:37:59 +0200

    add empty fake eventstore

---
+++ Backend/InitialEnterprise.Infrastructure/DDD/Event/EventStore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using InitialEnterprise.Infrastructure.DDD.Domain;
+using InitialEnterprise.Infrastructure.IoC;
+
+namespace InitialEnterprise.Infrastructure.DDD.Event
+{
+    public class EventStore : IEventStore, IInjectable
+    {
+        public async Task SaveEventAsync<TAggregate>(IDomainEvent @event) where TAggregate : IAggregateRoot
+        {
+            await Task.Run(() => { });
+        }
+
+        public async Task<IEnumerable<DomainEvent>> GetEventsAsync(Guid aggregateId)
+        {
+            return await Task.Run(() => { return new List<DomainEvent>(); });
+        }
+    }
+}
